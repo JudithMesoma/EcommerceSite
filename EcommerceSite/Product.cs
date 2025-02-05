e2a45392dbab4f3cd999a22cc86b5222219c43b1
Judith <joparaocha5242@conestogac.on.ca>
2025-02-04 21:10:14 -0500

    Added attributes and constructors to product class

--- EcommerceSite/Product.cs
+++ EcommerceSite/Product.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace EcommerceSite
 {
-    internal class Product
+    public class Product
     {
+        public int ProdID { get; set; }
+        public string ProdName { get; set; }
+        public decimal ItemPrice { get; set; }
+        public int StockAmount { get; set; }
+
+        // Constructor
+        public Product(int prodID, string prodName, decimal itemPrice, int stockAmount)
+        {
+            ProdID = prodID;
+            ProdName = prodName;
+            ItemPrice = itemPrice;
+            StockAmount = stockAmount;
+        }
+
+        // Method to increase stock
+        public void IncreaseStock(int amount)
+        {
+            if (amount > 0)
+                StockAmount += amount;
+        }
+
+        // Method to decrease stock
+        public void DecreaseStock(int amount)
+        {
+            if (amount > 0 && StockAmount - amount >= 0)
+                StockAmount -= amount;
+        }
     }
 }
