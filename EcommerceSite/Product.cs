cac36d44e20738c02396dddc94e37766c2d36194
JudithMesoma <169073898+JudithMesoma@users.noreply.github.com>
2025-02-04 21:21:51 -0500

    Update Product.cs

--- EcommerceSite/Product.cs
+++ EcommerceSite/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+//This is the base code(Product.cs), it has all the methods and attributes required
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
